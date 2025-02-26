b808480e65128a10728a455e2466b5b7199f53d6
ppomega <prateekveerbhan405@gmail.com>
2025-02-26 19:08:58 +0530

    g

--- business-logic/Db-Logic/animeinfo.js
+++ business-logic/Db-Logic/animeinfo.js
@@ -3,7 +3,6 @@ const connString = process.env.CONNECTION_STRING;
 const model = require("../../models/animeSeason");
 async function AnimeInfoFetch(name) {
   const AnimeInfo = model(name);
-  console.log(AnimeInfo);
   await mongoose
     .connect(connString)
     .then(() => {})
@@ -11,7 +10,8 @@ async function AnimeInfoFetch(name) {
       console.log(e);
     });
   const result = await AnimeInfo.find();
-  delete mongoose.connection.models["AnimeInfo"];
+  mongoose.deleteModel("AnimeInfo");
+  // delete mongoose.connection.models["AnimeInfo"];
 
   await mongoose.connection.close();
   return result;
