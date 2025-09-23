c60b1428f90a77fa3104169ca7227bbe14172f9d
ppomega <prateekveerbhan405@gmail.com>
2025-09-23 14:28:55 +0530

    native update

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const AnimeFetch = require("./business-logic/Db-Logic/animelistfetch");
 const AnimeInfoFetch = require("./business-logic/Db-Logic/animeinfo");
 const app = express();
 const PORT = process.env.PORT;
+var quality = 0;
 app.use(cors());
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running  http://localhost:${PORT}`);
 });
 app.get("/", (req, res) => {
   res.send("hello");
@@ -24,15 +25,22 @@ app.get("/assets", async (req, res) => {
 });
 app.get("/:disk/:name/:ep/:file", async (req, res) => {
   try {
+    var p = req.params.file;
+    if (p.charAt(0) == "i") {
+      p =
+        req.params.file.substring(0, 5) +
+        quality +
+        req.params.file.substring(6, req.params.file.length);
+    }
+    if (p.charAt(0) != "i" && p.charAt(0) != "o") {
+      p =
+        req.params.file.substring(0, 6) +
+        quality +
+        req.params.file.substring(7, req.params.file.length);
+    }
     console.log(req.params);
     const path =
-      req.params.disk +
-      "/" +
-      req.params.name +
-      "/" +
-      req.params.ep +
-      "/" +
-      req.params.file;
+      req.params.disk + "/" + req.params.name + "/" + req.params.ep + "/" + p;
     console.log(path);
     const chunk = await fetch(path);
     chunk.data.pipe(res);
@@ -40,7 +48,19 @@ app.get("/:disk/:name/:ep/:file", async (req, res) => {
 });
 app.get("/:disk/:cat/:name/:ep/:file", async (req, res) => {
   try {
-    console.log(req.params);
+    var p = req.params.file;
+    if (p.charAt(0) == "i") {
+      p =
+        req.params.file.substring(0, 5) +
+        quality +
+        req.params.file.substring(6, req.params.file.length);
+    }
+    if (p.charAt(0) != "i" && p.charAt(0) != "o") {
+      p =
+        req.params.file.substring(0, 6) +
+        quality +
+        req.params.file.substring(7, req.params.file.length);
+    }
     const path =
       req.params.disk +
       "/" +
@@ -50,8 +70,7 @@ app.get("/:disk/:cat/:name/:ep/:file", async (req, res) => {
       "/" +
       req.params.ep +
       "/" +
-      req.params.file;
-    console.log(path);
+      p;
     const chunk = await fetch(path);
     chunk.data.pipe(res);
   } catch (e) {}
@@ -62,4 +81,9 @@ app.get("/file", async (req, res) => {
     const file = await fetch(req.query.name);
     file.data.pipe(res);
   } catch (e) {}
+});
+app.get("/quality/:id", (req, res) => {
+  quality = req.params.id;
+  console.log(quality);
+  res.send(true);
 });
